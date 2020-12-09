7c03d8569801e68f0eae0ed96bb92ff5f50ac2c5
John <glennan20@gmail.com>
2020-12-09 15:40:53 -0500

    commit tree 30ba494b8872916e815be285fbb8714cac3e0e64

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
+require 'pry'
+
 def oxford_comma(array)
 
-end
+    if array.length == 2
+        return "#{array[0]} and #{array[1]}"
+    elsif array.length > 2
+        array[-1].insert(0, "and ")
+    end
+
+    array.join(', ')
+
+end 
